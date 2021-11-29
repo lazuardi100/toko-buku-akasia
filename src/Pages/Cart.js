a6e4ef9e78e02064da1b8be11cd0b7b841d7471a
devianwahyu <devianwahyu666@gmail.com>
2021-11-29 09:49:57 +0700

    Cart Local Storage

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -9,46 +9,31 @@ import CartItem from '../Components/Cart/CartItem';
 import ListCartItem from '../Components/Cart/ListCartItem';
 
 export default function Cart() {
-    let totalPrice = 0;
-    const tempData = [
-        {
-            "book_author": "Tere Liye",
-            "book_title": "Selamat Tinggal",
-            "book_price": 68000
-          },
-          {
-            "book_author": "Tere Liye",
-            "book_title": "Pulang - Pergi",
-            "book_price": 89000
-          },
-        {
-            "book_author": "Ilana Tan",
-            "book_title": "The Star and I",
-            "book_price": 99000
-          },
-    ];
-    return(
-        <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, }}>
-            <Toolbar />
-            <CssBaseline />
-            <Container maxWidth="md">
-            <h2>Keranjang Belanja</h2>
-                <ListCartItem>
-                {tempData.map((data, id) => (
-                    <CartItem
-                    key={id}
-                    book_author={data['book_author']}
-                    book_title={data['book_title']}
-                    book_price={data['book_price']}
-                />
-                ))}
-                </ListCartItem>
-                {tempData.forEach(function(value){
-                    totalPrice += value.book_price;
-                })}
-                <h3> Total Pembayaran Rp.{totalPrice} </h3>
-                <Button alignCenter>Lanjutkan Pembelian</Button>
-            </Container>
-        </Box>
-    )
+  let totalPrice = 0;
+  const cart = JSON.parse(localStorage.getItem('tba-cart'));
+
+  return (
+    <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, }}>
+      <Toolbar />
+      <CssBaseline />
+      <Container maxWidth="md">
+        <h2>Keranjang Belanja</h2>
+        <ListCartItem>
+          {cart.map((data, id) => (
+            <CartItem
+              key={id}
+              book_author={data['book_author']}
+              book_title={data['book_title']}
+              book_price={data['book_price']}
+            />
+          ))}
+        </ListCartItem>
+        {cart.forEach(function (value) {
+          totalPrice += value.book_price;
+        })}
+        <h3> Total Pembayaran Rp.{totalPrice} </h3>
+        <Button alignCenter>Lanjutkan Pembelian</Button>
+      </Container>
+    </Box>
+  )
 }
