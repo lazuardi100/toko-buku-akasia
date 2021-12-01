6121169c4ee86ac1e73ff3f4df97df488042e91a
Dion Ricky Saputra <dionrickysptr@gmail.com>
2021-12-01 13:16:11 +0000

    Delete Book from Cart + Cleaning Fixes #30

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -5,38 +5,30 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-/* import Box from '@mui/material/Box'; */
 
 class CartItem extends React.Component {
-    render() {
-        return (
-        <Grid item sm={1} md={1}>
+  render() {
+    return (
+      <Grid item sm={1} md={1}>
         <Card>
-          {/*<Box sx={{ bgcolor: '#cfe8fc', height: '240px', display: 'flex' }}/>
-           <CardMedia
-            component="img"
-            height="140"
-            image={this.props.image_url}
-            alt={this.props.book_title}
-          /> */}
           <CardContent>
             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              { this.props.book_author }
+              {this.props.book_author}
             </Typography>
             <Typography gutterBottom variant="h5" component="div">
-              { this.props.book_title }
+              {this.props.book_title}
             </Typography>
             <Typography sx={{ fontSize: 14, fontWeight: 'medium' }} color="text.secondary">
-              Rp. { this.props.book_price }
+              Rp. {this.props.book_price}
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Hapus</Button>
+            <Button size="small" onClick={this.props.delete_clicked.bind(this, this.props.book_id)}>Hapus</Button>
           </CardActions>
         </Card>
       </Grid>
-      )
-    }
+    )
+  }
 }
 
 export default CartItem;
--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -8,43 +8,93 @@ import Button from '@mui/material/Button';
 import CartItem from '../Components/Cart/CartItem';
 import ListCartItem from '../Components/Cart/ListCartItem';
 
-export default function Cart() {
-  let totalPrice = 0;
-  const cart = JSON.parse(localStorage.getItem('tba-cart'));
-  let isbnData = ''
+class Cart extends React.Component {
+  constructor(props) {
+    super(props);
 
-  cart.forEach((data) => {
-    isbnData += `${data['isbn']};`;
-  })
+    this.cartItemDelete = this.cartItemDelete.bind(this);
 
-  let checkout = `/checkout?id=${isbnData}`;
-  console.log(checkout)
+    this.state = {
+      cart: [],
+      total_price: 0,
+      continue_link: '/checkout'
+    }
+  }
 
-  return (
-    <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, }}>
-      <Toolbar />
-      <CssBaseline />
-      <Container maxWidth="md">
-        <h2>Keranjang Belanja</h2>
-        {cart ?
-          <div>
-            <ListCartItem>
-              {cart.map((data, id) => (
-                <CartItem
-                  key={id}
-                  book_author={data['book_author']}
-                  book_title={data['book_title']}
-                  book_price={data['book_price']}
-                />
-              ))}
-            </ListCartItem>
-            {cart.forEach(function (value) {
-              totalPrice += value.book_price;
-            })}
-            <h3> Total Pembayaran = Rp.{totalPrice} </h3>
-            <Button alignCenter href={checkout}>Lanjutkan Pembelian</Button>
-          </div> : "Kosong :("}
-      </Container>
-    </Box>
-  )
+  componentDidMount() {
+    const cart = JSON.parse(localStorage.getItem('tba-cart'));
+    let book_id_in_cart = []
+
+    cart.forEach((data) => {
+      book_id_in_cart.push(data['book_id']);
+    })
+
+    this.setState({
+      cart: cart,
+      total_price: this.calculateTotalPrice(cart),
+      continue_link: '/checkout?id=' + book_id_in_cart.join(';')
+    })
+  }
+
+  updateCartLocalStorage(cart_items) {
+    if (Array.isArray(cart_items)) {
+      localStorage.setItem('tba-cart', JSON.stringify(cart_items));
+    } else {
+      console.error('LocalStorage Error: Saved cart data type is ' + cart_items.constructor.name);
+    }
+  }
+
+  calculateTotalPrice(cart_items) {
+    let total_price = 0
+
+    cart_items.forEach((data) => {
+      total_price += data['book_price'];
+    });
+
+    return total_price
+  }
+
+  cartItemDelete(book_id, e) {
+    const cart = this.state.cart;
+    let newCart = cart.filter(c => c['book_id'] !== book_id)
+
+    this.setState({
+      cart: newCart,
+      total_price: this.calculateTotalPrice(newCart)
+    });
+
+    this.updateCartLocalStorage(newCart);
+  }
+
+  render() {
+    return (
+      <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, }}>
+        <Toolbar />
+        <CssBaseline />
+        <Container maxWidth="md">
+          <h2>Keranjang Belanja</h2>
+          {this.state.cart ?
+            <div>
+              <ListCartItem>
+                {this.state.cart.map((data, id) => (
+                  <CartItem
+                    key={data['book_id']}
+                    book_id={data['book_id']}
+                    book_author={data['book_author']}
+                    book_title={data['book_title']}
+                    book_price={data['book_price']}
+                    delete_clicked={this.cartItemDelete}
+                  />
+                ))}
+              </ListCartItem>
+              <h3> Total Pembayaran = Rp.{this.state.total_price} </h3>
+              <Button href={this.state.continue_link}>Lanjutkan Pembelian</Button>
+            </div> : 
+            <p> Kosong :( </p>}
+        </Container>
+      </Box>
+    )
+  }
 }
+
+export default Cart;
